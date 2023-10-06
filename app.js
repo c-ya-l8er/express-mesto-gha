028869c23f326591cec9a9d566c25ed8c738a9e6
c-ya-l8er <c-ya-l8er@ya.ru>
2023-10-07 02:55:47 +0300

    ПР 13 feat: прописал ошибки

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const { PORT = 3000, MONGO_URL = "mongodb://localhost:27017/mestodb" } = process
 const app = express();
 
 app.use(express.json());
+app.use((req, res) => {
+  res.status(404).send({ message: "Ошибка - 404 Страница не найдена" });
+});
 
 app.use((req, res, next) => {
   res.status(404).send({ message: "Ошибка - 404 Страница не найдена" });
