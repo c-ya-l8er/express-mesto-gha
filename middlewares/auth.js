b4fc80e7fbeb863df2d6d55d74e6e4db82647345
c-ya-l8er <c-ya-l8er@ya.ru>
2023-10-30 14:02:33 +0300

    ПР 14 fix: исправил замечания по ревью

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,8 @@
 const jwt = require('jsonwebtoken');
-const statusCodes = require('../utils/constants').HTTP_STATUS;
-// const UNAUTHORIZED = require('../errors/Unauthorized');
+const UNAUTHORIZED = require('../errors/Unauthorized');
 
-const handleAuthError = (res) => {
-  res
-    .status(statusCodes.UNAUTHORIZED)
-    .send({ message: 'Необходима авторизация' });
+const handleAuthError = (res, next) => {
+  next(new UNAUTHORIZED('Необходима авторизация'));
 };
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
@@ -14,7 +11,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    return handleAuthError(res, next);
   }
 
   const token = extractBearerToken(authorization);
@@ -23,7 +20,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'super-puper-secret');
   } catch (error) {
-    return handleAuthError(res);
+    return handleAuthError(res, next);
   }
 
   req.user = payload;
